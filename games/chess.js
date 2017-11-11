e3a5431eb8e1f62a17bf2de5d6be348a11b0d3a8
Sebastian Kylander <sebastian@kylander.me>
2017-11-11 03:35:32 +0100

    Embed chess game messages

--- games/chess.js
+++ games/chess.js
@@ -12,8 +12,49 @@ class ChessGame
 		this.playerOne = playerOne;
 		this.playerTwo = playerTwo;
 		this.channel = channel;
+		this.turn = 0;
 
-		channel.send(playerOne + " started a chess game with " + playerTwo);
+		// Create embedded message
+		this.embed = new DISCORD.RichEmbed();
+		this.embed.setColor([255, 255, 255]);
+		this.embed.setDescription(playerOne + " vs " + playerTwo);
+		this.embed.setFooter("Footer");
+		this.embed.setImage("");
+		this.embed.setThumbnail(playerOne.user.avatarURL);
+		this.embed.setTitle("Title");
+
+		// Send and store message
+		channel.send(this.embed).then(msg =>
+		{
+			this.msg = msg;
+		});
+
+		IMGUR_UPLOADER(FS.readFileSync("res/chessboard.jpg"), {title: "Chessboard"}).then(data =>
+		{
+			if (data)
+			{
+				this.embed.setFooter("Footer", data.link);
+				this.embed.setImage(data.link);
+				if (this.msg)
+					this.msg.edit(this.embed);
+			}
+		});
+
+		IMGUR_UPLOADER(FS.readFileSync("res/cancelled.jpg"), {title: "Cancelled"}).then(data =>
+		{
+			this.imageCancelled = data.link;
+		});
+	}
+
+	nextTurn()
+	{
+		this.turn = (this.turn + 1) % 2;
+		if (this.msg)
+		{
+			let player = (this.turn == 0) ? this.playerOne : this.playerTwo;
+			this.embed.setThumbnail(player.user.avatarURL);
+			this.msg.edit(this.embed);
+		}
 	}
 }
 
@@ -62,15 +103,37 @@ function end(args, member, channel)
 	if (i != -1)
 	{
 		let chess_game = chess_games_[i];
-		
+
+		// Delete embedded message
+		if (chess_game.msg)
+		{
+			chess_game.embed.setDescription("Game was cancelled");
+			chess_game.embed.setFooter("");
+			chess_game.embed.setImage("");
+			if (chess_game.imageCancelled)
+				chess_game.embed.setThumbnail(chess_game.imageCancelled);
+
+			// Edit message
+			chess_game.msg.edit(chess_game.embed);
+		}
+
 		// Cancel game
-		chess_game.channel.send("Chess game between " + chess_game.playerOne + " and " +
-			chess_game.playerTwo + " has been cancelled");
 		return chess_games_.splice(i);
 	}
 
 	// No ongoing game including specified member
 	channel.send(member + " is not in any game right now");	
+}
+
+/* Next turn */
+function nextTurn(args, member, channel)
+{
+	let i = gameIndexFromMember(member);
+	if (i == -1)
+		return channel.send(member + " is not in any game right now");
+
+	let chess_game = chess_games_[i];
+	chess_game.nextTurn();
 }
 
 /* Export module objects */
@@ -87,5 +150,7 @@ module.exports =
 			start(args, member, channel);
 		else if (arg0 == "end")
 			end(args, member, channel);
+		else if (arg0 == "nextTurn")
+			nextTurn(args, member, channel);
 	}
 }
